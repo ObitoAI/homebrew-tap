191944ff3982767968e77b0666eda0780d9d9316
ewu-like-coding <ewu@obito-ai.com>
2026-09-18 17:25:06 -0700

    obito 0.0.1: fill in real darwin checksums

--- Formula/obito.rb
+++ Formula/obito.rb
@@ -7,10 +7,10 @@ class Obito < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ObitoAI/obito-releases/releases/download/v0.0.1/obito_v0.0.1_darwin_arm64.tar.gz"
-      sha256 "ARM64_CHECKSUM"
+      sha256 "e6b580ba121c38bdb3a1e0b976c1e91ed99c7ca3725141c088651884f3e8921e"
     else
       url "https://github.com/ObitoAI/obito-releases/releases/download/v0.0.1/obito_v0.0.1_darwin_amd64.tar.gz"
-      sha256 "AMD64_CHECKSUM"
+      sha256 "0c4a3e3dc2bb358b6e456388f4585a776ae1ebe6fdc9130c245a8595a603c411"
     end
   end
 
